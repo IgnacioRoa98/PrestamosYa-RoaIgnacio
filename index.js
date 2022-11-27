5589e49941736d5e0f2a09669b0b676db1501c12
Ignacio Roa <ignacioroa98@gmail.com>
2022-11-27 17:16:46 -0300

    prittier

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-
 /* Calculadora de intereses de financiera */
 
-let calculoInteres = function (){
-    let interesAPagar 
-    let cuotasUna = 15
-    let cuotasTres = 30
-    let cuotasSeis = 45
-    let cuotasNueve = 60
-    let cuotasDoce = 100
-   let montoSolicitado = prompt ("Ingrese el monto que desea que le prestemos: ")
-   let cuotasSolicitadas = prompt ("Ingrese el numero de cuotas en el que desea devolverlo")
-   
-    switch (cuotasSolicitadas) {
-        case "1":
-           return (montoSolicitado*cuotasUna)/100
-        break;
-        case "3":
-            return (montoSolicitado*cuotasTres)/100
-            
-        break;
-        case "6":
-            return (montoSolicitado*cuotasSeis)/100
-        break;
-        case "9":
-            return (montoSolicitado*cuotasNueve)/100
-        break;
-        case "12":
-            return (montoSolicitado*cuotasDoce)/100
-        break;
-        default:
-            return ("Ingrese numero de cuotas validas")
-        break;
-                    }  
-                    
-         }  
-    let interesAPagar = calculoInteres()
-    alert ("Su interes es de: " + interesAPagar)
-  
+let calculoInteres = function () {
+  let interesAPagar;
+  let cuotasUna = 15;
+  let cuotasTres = 30;
+  let cuotasSeis = 45;
+  let cuotasNueve = 60;
+  let cuotasDoce = 100;
+  let montoSolicitado = prompt("Ingrese el monto que desea que le prestemos: ");
+  let cuotasSolicitadas = prompt(
+    "Ingrese el numero de cuotas en el que desea devolverlo"
+  );
+
+  switch (cuotasSolicitadas) {
+    case "1":
+      return (montoSolicitado * cuotasUna) / 100;
+      break;
+    case "3":
+      return (montoSolicitado * cuotasTres) / 100;
+
+      break;
+    case "6":
+      return (montoSolicitado * cuotasSeis) / 100;
+      break;
+    case "9":
+      return (montoSolicitado * cuotasNueve) / 100;
+      break;
+    case "12":
+      return (montoSolicitado * cuotasDoce) / 100;
+      break;
+    default:
+      return "Ingrese numero de cuotas validas";
+      break;
+  }
+};
+let interesAPagar = calculoInteres();
+alert("Su interes es de: " + interesAPagar);
